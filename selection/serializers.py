6168abcbf6f937688dd54c9ad40cb46cb6eccc9b
Vadim <niras.sp@gmail.com>
2022-12-17 08:57:54 +0700

    fix selection/serializers

--- selection/serializers.py
+++ selection/serializers.py
@@ -27,15 +27,20 @@ class SelectionUDSerializer(serializers.ModelSerializer):
 
 class SelectionCreateSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(required=False, read_only=True)
-
     def _get_owner(self):
         request = self.context.get('request', None)
-        if request.user.role not in ('moderator', 'admin'):
-            return request.user
+        return request.user
+
+    def _can_create(self):
+        request = self.context.get('request', None)
+        if request.user.role in ('moderator', 'admin'):
+            return True
 
     def create(self, validated_data):
-        if self._get_owner():
+        if "owner" not in validated_data:
             validated_data['owner'] = self._get_owner()
+        elif "owner" in validated_data and not self._can_create():
+            raise PermissionError("Not enough permissions")
         return super().create(validated_data)
 
     class Meta:
